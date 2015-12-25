cb0560eafb4fbf7b548936f4a4b4c6bb8e9a9154
Eric Hardy <Eric.Hardy@univ-brest.fr>
2015-12-25 14:35:32 +0100

    try du fix hover on menus

--- src/rmi/site/skins/rmi.site/rmi-site.js
+++ src/rmi/site/skins/rmi.site/rmi-site.js
@@ -1,4 +1,13 @@
 
+/*
+http://stackoverflow.com/questions/9765098/plone-and-twitter-bootstrap
+https://github.com/CWSpear/bootstrap-hover-dropdown
+*/
+
 $(document).ready(function() {
     $("body").addClass("rmi");
+    $(".dropdown").hover(
+            function(){ $(this).addClass('open') },
+            function(){ $(this).removeClass('open') }
+        );
 });
